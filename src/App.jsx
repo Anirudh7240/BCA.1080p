7a502e7c06a35ff877fdd5734328d53af352b1e6
Anirudh <anirudh@example.com>
2026-03-31 14:21:22 +0530

    Fix unused variables linting errors breaking CI

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import LoadingScreen from './components/LoadingScreen';
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
